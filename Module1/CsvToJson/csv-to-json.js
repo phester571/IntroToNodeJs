076e5226ff6f3256af0a85506f31404700082c27
Philip Murphy <gitacctmail@gmail.com>
2017-12-23 21:50:42 -0500

    Add process.exit calls

--- Module1/CsvToJson/csv-to-json.js
+++ Module1/CsvToJson/csv-to-json.js
@@ -11,11 +11,18 @@ convertFile = (csvFilePath=path.join(__dirname, 'customer-data.csv')) => {
         buff += JSON.stringify(jsonObj, null, 2)
     })
     .on('done', (error) => {
-        if (error) return `Error encountered: ${error}`
+        if (error) {
+            console.log(`Error encountered: ${error}`)
+            process.exit(1)
+        }
         let outFile = path.basename(csvFilePath, '.csv') + '.json'
         fs.writeFile(path.join(__dirname, outFile), buff, (error) => {
-            if (error) console.log(`Error writing file: ${error}`)
-            else console.log(`CSV to JSON conversion completed.  Results stored in ${outFile}`)
+            if (error) {
+                console.log(`Error writing file: ${error}`)
+                process.exit(1)
+            }
+            console.log(`CSV to JSON conversion completed.  Results stored in ${outFile}`)
+            process.exit(0)
         })
     })
 }
